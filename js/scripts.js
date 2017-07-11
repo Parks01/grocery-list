e553e49ac572f618268661fd253f6730d67b1bc2
Andrew Parks and Michael Woldemedihin <andrew@>
2017-07-11 11:38:08 -0700

    change foreach loop into forloop

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
 
-  var foods = ["produce","meat", "bread","drinks","quicksauce"]; {
-
-    foods.forEach(function(food) {
-    var userInput = ($("input#" + food).val()).toUpperCase();
-    $("ul#list").append("<li>" + userInput + "</li>");
-  });
+   var foods = ["produce","meat", "bread","drinks","quicksauce"];
+  // foods.forEach(function(food){
+  //   $("ul#list").append("<li>" + food + "</li>");
+  // })
+  for(var i = 0; i < 5; i++) {
+    $("ul#list").append("<li>" + foods[i] + "</li>");
+  }
 
   $("#formOne").hide();
 
